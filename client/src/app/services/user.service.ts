c1ee2a6ae78a8886d52b709f4e6544a653a6a806
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2022-02-12 18:35:08 -0500

    form submits changes user value

---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private _user: User = {
+    name:"",
+    email:"",
+    phone:"",
+    address:""
+  };
+
+  constructor() { }
+
+  set user(user: User){
+    this._user=user;
+  }
+
+  get user(){
+    return this._user;
+  }
+}
