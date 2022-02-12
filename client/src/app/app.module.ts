323f0ce58631d81a49b9482a4ec8dbdda2cab730
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2022-02-12 13:48:05 -0500

    Structure de fichiers retravaillee

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './modules/app-routing.module';
+import { AppComponent } from '../app/pages/app/app.component'
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
