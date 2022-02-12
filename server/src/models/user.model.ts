87ef1f858a802c30d3834feecef228ece5ff1008
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2022-02-12 13:12:23 -0500

    loopback4 server with user model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  mail: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
