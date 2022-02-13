bf7ac2bb13fa524b3190eec9a0dc1e874bb57660
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2022-02-13 09:53:11 -0500

    ajouter produit fonctionne

--- client/src/app/pages/add-article/add-article.component.ts
+++ client/src/app/pages/add-article/add-article.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-add-article',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddArticleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private products: ProductsService, private fb: FormBuilder, private router:Router) { }
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    timer: [0, [Validators.required]],
+    category: ['', [Validators.required]],
+    distance: [0, [Validators.required]],
+  });
 
   ngOnInit(): void {
   }
   
+  onSubmit(){
+    this.products.addProduct({
+      name: this.myForm.value.name,
+      timer: this.myForm.value.timer,
+      category: this.myForm.value.category,
+      distance: this.myForm.value.distance,
+      current: 0,
+      isExpanded: false,
+    })
+    this.router.navigate(['/main']);
+  }
 }
